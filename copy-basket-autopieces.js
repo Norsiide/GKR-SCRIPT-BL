0efc28ac42904e24c9d917aeb9d9b9a96910488d
Norsiide <larivierelaurent94@gmail.com>
2026-09-15 16:48:00 +0200

    Update script name for GKR application

--- copy-basket-autopieces.js
+++ copy-basket-autopieces.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TopAutoPieces (INTERCAR) copy basket -> gkr.norsiide.be
+// @name         TopAutoPieces copy basket -> app.gkr.be
 // @namespace    http://tampermonkey.net/
 // @version      4.2
 // @description  Transfert automatique d'articles, références exactes (Code), désignations épurées "Marque - Nom de la pièce" et prix (HTVAC) depuis TopAutoPieces vers GKR
